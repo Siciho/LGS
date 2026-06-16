db8ebff80f4f9f0e887a5dcb9899d1f0a06acb4a
Coşkun GÜNEŞ <coskungunes11@gmail.com>
2026-06-16 23:01:08 +0300

    fix(android): change app name, update logo, resolve white screen, and build clean apk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.student.app',
-  appName: 'Öğrenci+',
+  appName: 'LGS Asistanım',
   webDir: 'dist',
   // YENİ EKLENEN BÖLÜM
   plugins: {
--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY  // ✅ DÜZELTİLDİ
+const fallbackUrl = "https://wvxuysrijpronuldpsyp.supabase.co";
+const fallbackAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2eHV5c3JpanByb251bGRwc3lwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcyNjIwODksImV4cCI6MjA3MjgzODA4OX0.5E7muuJ7Jv7g0p6pgeweGRSirSZWbWjZmY3UuYBIsBc";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || fallbackUrl
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || fallbackAnonKey  // ✅ DÜZELTİLDİ
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Supabase URL veya ANON KEY bulunamıyor. Lütfen .env dosyanızı kontrol edin.')
